f8904743340b3a9fae978ce762a6e3938ece5544
Fayaz Rafeek <fayazmrafeek@gmail.com>
2023-01-21 16:00:26 +0530

    Update links

--- src/components/layout/partials/FooterNav.js
+++ src/components/layout/partials/FooterNav.js
@@ -19,7 +19,7 @@ const FooterNav = ({
     >
       <ul className="list-reset">
         <li>
-          <Link to="#0">Contact</Link>
+          <Link to="https://api.whatsapp.com/send?phone=+919497521899">Contact</Link>
         </li>
         <li>
           <Link to="#0">About us</Link>
